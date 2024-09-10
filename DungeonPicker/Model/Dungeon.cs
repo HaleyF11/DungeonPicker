099787dcffe457ae79b5fa74d95394bb95d84afe
Haley <fredianig11@gmail.com>
2024-09-10 14:51:49 +0200

    Add project files.

---
+++ DungeonPicker/Model/Dungeon.cs
@@ -0,0 +1,10 @@
+﻿namespace DungeonPicker
+{
+    public class Dungeon
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Expansion { get; set; }
+
+    }
+}
---
+++ DungeonPicker/SeedData.cs
@@ -0,0 +1,6 @@
+﻿namespace DungeonPicker
+{
+    public class SeedData
+    {
+    }
+}
